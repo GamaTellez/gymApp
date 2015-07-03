163d12f117e5d814e734564416c33d5bf737f5e2
Gamaliel Tellez <gamatellez@gmail.com>
2015-07-03 11:09:21 -0600

    added toolbar to picker view in profile

---
+++ AppAppearance.h
@@ -0,0 +1,13 @@
+//
+//  AppAppearance.h
+//  MyGymApp
+//
+//  Created by Gamaliel Tellez on 7/2/15.
+//  Copyright (c) 2015 Gamaliel Tellez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppAppearance : NSObject
+
+@end
---
+++ AppAppearance.m
@@ -0,0 +1,13 @@
+//
+//  AppAppearance.m
+//  MyGymApp
+//
+//  Created by Gamaliel Tellez on 7/2/15.
+//  Copyright (c) 2015 Gamaliel Tellez. All rights reserved.
+//
+
+#import "AppAppearance.h"
+
+@implementation AppAppearance
+
+@end
